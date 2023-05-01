e2c5110dcb1a6c706deb9a51481e94979ec0a5b3
Katsiaryna Anashkevich <sweetsyammy2003@gmail.com>
2023-05-01 14:24:58 +0300

    Добавьте файлы проекта.

---
+++ Chat/Controllers/WeatherForecastController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chat.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        
+    }
+}
