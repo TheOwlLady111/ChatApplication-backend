9d8abbdeb40bf33d979973a15f57faa6c8c1faf7
Katsiaryna Anashkevich <sweetsyammy2003@gmail.com>
2023-05-02 00:04:05 +0300

    bll/implement chat hub

--- Chat.BLL/Hubs/ChatHub.cs
+++ Chat.BLL/Hubs/ChatHub.cs
@@ -1,9 +1,37 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Chat.BLL.Contracts;
+using Chat.BLL.ViewModels;
+using Microsoft.AspNetCore.SignalR;
 
 namespace Chat.BLL.Hubs;
 
 public class ChatHub : Hub
-
 {
-    
+    private readonly IMessageService _messageService;
+
+    public ChatHub(IMessageService messageService)
+    {
+        _messageService = messageService;
+    }
+
+    public async Task SendMessageAsync(CreateMessageViewModel message)
+    {
+        var createdMessage = await _messageService.CreateMessagesAsync(message);
+
+        await Clients.All.SendAsync("ReceiveMessage", createdMessage);
+    }
+
+    public async Task UpdateMessageAsync(UpdateMessageViewModel message)
+    {
+        await _messageService.UpdateMessageAsync(message);
+        var updatedMessage = _messageService.GetMessageAsync(message.Id);
+
+        await Clients.All.SendAsync("ReceiveMessage", updatedMessage);
+    }
+
+    public async Task DeleteMessageAsync(int id)
+    {
+        await _messageService.DeleteMessageAsync(id);
+
+        await Clients.All.SendAsync("ReceiveMessage", $"Message with id {id} is deleted!");
+    }
 }
