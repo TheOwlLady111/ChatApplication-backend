369d1ffe6f40c7753d5adfa97054dd72fe2a5bc1
Katsiaryna Anashkevich <sweetsyammy2003@gmail.com>
2023-05-01 17:54:02 +0300

    dal/entities changes

--- Chat.DAL/Entities/BaseEntity.cs
+++ Chat.DAL/Entities/BaseEntity.cs
@@ -4,6 +4,7 @@ namespace Chat.DAL.Entities;
 
 public class BaseEntity : IBaseEntity
 {
+    public int Id { get; set; }
     public DateTime CreatedAtUtc { get; set; }
     public DateTime? UpdatedAtUtc { get; set; }
 }
--- Chat.DAL/Entities/Message.cs
+++ Chat.DAL/Entities/Message.cs
@@ -2,7 +2,6 @@
 
 public class Message : BaseEntity
 {
-    public int Id { get; set; }
     public string Text { get; set; }
 
     public int UserId { get; set; }
