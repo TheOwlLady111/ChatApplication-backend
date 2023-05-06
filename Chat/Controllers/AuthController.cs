7eb8ad63f323da29715fb4d555601820174c57ef
Katsiaryna Anashkevich <sweetsyammy2003@gmail.com>
2023-05-06 20:55:03 +0300

    api/add controllers

---
+++ Chat/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Chat.Api.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
