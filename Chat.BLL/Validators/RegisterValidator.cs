5612f803888ec95bafe8d54520552dd3140e24a4
Katsiaryna Anashkevich <sweetsyammy2003@gmail.com>
2023-05-06 20:43:51 +0300

    bll/add validators

--- Chat.BLL/Validators/RegisterValidator.cs
+++ Chat.BLL/Validators/RegisterValidator.cs
@@ -1,6 +1,22 @@
-﻿namespace Chat.BLL.Validators;
+﻿using Chat.BLL.ViewModels;
+using FluentValidation;
 
-public class RegisterValidator
+namespace Chat.BLL.Validators;
+
+public class RegisterValidator : AbstractValidator<RegisterViewModel>
 {
-    
+    public RegisterValidator()
+    {
+        RuleFor(x => x.UserName)
+            .NotEmpty()
+            .MaximumLength(150);
+
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .MaximumLength(8);
+
+        RuleFor(x => x.ConfirmPassword)
+            .NotEmpty()
+            .MaximumLength(8);
+    }
 }
