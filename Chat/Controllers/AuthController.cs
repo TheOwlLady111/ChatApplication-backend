0a3f0e5389e3c3f416585bd084c7b832cff24a0c
Katsiaryna Anashkevich <sweetsyammy2003@gmail.com>
2023-05-06 21:03:36 +0300

    api/add auth endpoints

--- Chat/Controllers/AuthController.cs
+++ Chat/Controllers/AuthController.cs
@@ -1,12 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Chat.BLL.Contracts;
+using Chat.BLL.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Chat.Api.Controllers
 {
-    public class AuthController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
         {
-            return View();
+            _authService = authService;
+        }
+
+        [HttpPost]
+        [Route("registration")]
+        public async Task<ActionResult<SuccessUserLoginViewModel>> RegisterAsync([FromBody] RegisterViewModel registerViewModel)
+        {
+            var result = await _authService.RegisterAsync(registerViewModel);
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<ActionResult<SuccessUserLoginViewModel>> LoginAsync([FromBody] LoginViewModel loginViewModel)
+        {
+            var result = await _authService.LoginAsync(loginViewModel);
+
+            return Ok(result);
         }
     }
 }
